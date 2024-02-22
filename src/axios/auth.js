e7fe36cb2d972246649f5a890ce1cf954a43e257
leejieun2023 <lge9380@naver.com>
2024-02-22 15:56:21 +0900

    7단계 하던 중

--- src/axios/auth.js
+++ src/axios/auth.js
@@ -1,5 +1,19 @@
 import axios from "axios";
 
-export const authApi = axios.create({});
+export const authApi = axios.create({
+    baseURL: "https://moneyfulpublicpolicy.co.kr/",
+    headers: {
+        "Content-Type": "application/json"
+    }
+});
 
-authApi.interceptors.request.use();
+authApi.interceptors.request.use((config) => {
+    if (config.url.includes('user')) {
+        const accessToken = localStorage.getItem('accessToken')
+        if (!accessToken) {
+            alert("인증이 필요합니다.");
+            return Promise.reject("인증이 필요합니다.");
+        };
+        //congig.headers : 어떻게 저장하더라 .......
+    }
+})
--- src/axios/comments.js
+++ src/axios/comments.js
@@ -1,10 +1,28 @@
 import axios from "axios";
 import { authApi } from "./auth";
 
-const commentsAxios = axios.create({});
+const commentsAxios = axios.create({
+    baseURL: "http://localhost:3001/comments",
+    timeout: 1500
+});
 
-commentsAxios.interceptors.request.use();
+commentsAxios.interceptors.request.use(async (config) => {
+    try {
 
-commentsAxios.interceptors.response.use();
+    }
+    catch (error) {
+
+    }
+});
+
+commentsAxios.interceptors.response.use((response) => {
+    console.log(”요청 성공입니다.”);
+    return response;
+},
+    (error) => {
+        console.log(”요청 실패입니다.”);
+        return Promise.reject(error);
+    }
+);
 
 export default commentsAxios;
